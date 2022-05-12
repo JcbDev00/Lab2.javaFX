9259c4a27401df5191cf4f8ffee6e667e8c59efd
JcbDev00 <59119576+JcbDev00@users.noreply.github.com>
2022-05-12 18:56:42 +0200

    Add files via upload

--- Lab3/src/main/java/com/example/lab3/HelloController.java
+++ Lab3/src/main/java/com/example/lab3/HelloController.java
@@ -2,21 +2,60 @@ package com.example.lab3;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+
 public class HelloController {
     public TextField txtLicznik;
-    int counter = 0;
+    public CheckBox typeA;
+    public TextField expField;
+    public CheckBox typeB;
+    public TextField heightField;
+    public TextField radiusField;
+    public TextField sizeField;
+    public TextField weightField;
+
     public Button btnMain;
 
+    int counter = 0;
+    float radius = 0;
+    float heigtht = 0;
+    float size = 0;
+    double m = 7.85;
+    double area = 0;
+
+    double price = 0;
+    double weight = 0;
+    boolean type = false;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        ++counter;
-        System.out.println(counter);
-        txtLicznik.setText("" + counter);
+        radius = Float.parseFloat(radiusField.getText());
+        heigtht = Float.parseFloat(heightField.getText());
+        size = Float.parseFloat(sizeField.getText());
+
+        System.out.println(""+ heigtht+ " "+ radius + " " + size);
+
+
+        if (typeA.isSelected()) {
+            area = (2*Math.PI)*radius*heigtht;
+            System.out.println(area);
+            weight = size * m;
+            price = (area/100)*5;
+            weightField.setText(weight + " kg");
+            expField.setText(price + " PLN");
+
+        } else if(typeB.isSelected()) {
+            area = (2*Math.PI)*radius*heigtht;
+            System.out.println(area);
+            weight = size * m;
+            price = (area/100)*10;
+            weightField.setText(weight + " kg");
+            expField.setText(price + " PLN");
+        }
     }
 }
